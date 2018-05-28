68fbe9f7e57996622640c464f481fafd44b29288
eyave <elmeryachin@gmail.com>
2018-05-28 14:35:56 -0400

    correccion pedidoObjeto

--- src/pages/response/response-add-pedido.ts
+++ src/pages/response/response-add-pedido.ts
@@ -1,6 +1,6 @@
 import { ResponsePedido} from "../request/request-pedido";
 
 export class ResponseAddPedido{
-  constructor(public pedidoObjeto:ResponsePedido, public respuesta:boolean, public mensaje:string){
+  constructor(public transaccionObjeto:ResponsePedido, public respuesta:boolean, public mensaje:string){
   }
 }
